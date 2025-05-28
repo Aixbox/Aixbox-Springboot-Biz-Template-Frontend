b13ac43d8f485e4948092a414c5e8c4f7aaf4021
Aixbox <1481051129@qq.com>
2025-05-28 23:34:31 +0800

    feat(web-antd): 新增用户管理功能
    - Added @ant-design/icons-vue version 7.0.1 to package.json and pnpm-lock.yaml.
    - Introduced InBoxIcon and ExcelIcon in iconify-offline module for enhanced icon support.

--- packages/icons/src/iconify-offline/index.ts
+++ packages/icons/src/iconify-offline/index.ts
@@ -1,8 +1,10 @@
 import { createIconifyOfflineIcon } from '@vben-core/icons';
 
+import inboxIcon from '@iconify/icons-ant-design/inbox-outlined';
 import folderIcon from '@iconify/icons-flat-color-icons/folder';
 import menuIcon from '@iconify/icons-material-symbols/menu';
 import okButtonIcon from '@iconify/icons-mdi/button-pointer';
+import excelIcon from '@iconify/icons-vscode-icons/file-type-excel';
 
 // 菜单类型 目录/按钮/菜单
 export const FolderIcon = createIconifyOfflineIcon(
@@ -16,4 +18,17 @@ export const OkButtonIcon = createIconifyOfflineIcon(
 export const MenuIcon = createIconifyOfflineIcon(
   'material-symbols:menu',
   menuIcon,
+);
+
+// 拖拽上传图标
+export const InBoxIcon = createIconifyOfflineIcon(
+  'ant-design:inbox-outlined',
+  inboxIcon,
+);
+
+// 用户管理 导入
+// Excel图标
+export const ExcelIcon = createIconifyOfflineIcon(
+  'vscode-icons:file-type-excel',
+  excelIcon,
 );
