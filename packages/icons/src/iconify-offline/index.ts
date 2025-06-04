3a364ca00c8a50efe089107d1f68457f5e824180
Aixbox <1481051129@qq.com>
2025-06-04 11:13:08 +0800

    feat(common-ui): integrate CodeMirror dependencies and enhance icon support
    - Added various CodeMirror language packages for HTML, Java, JavaScript, SQL, Vue, and XML to enhance code editing capabilities.
    - Included the theme-one-dark package for improved UI aesthetics.
    - Updated package.json to include codemirror and vue-codemirror6 for better integration.
    - Enhanced iconify-offline module by adding new icons for Java, SQL, TypeScript, XML, and Vue to improve user interface experience.

--- packages/icons/src/iconify-offline/index.ts
+++ packages/icons/src/iconify-offline/index.ts
@@ -1,9 +1,15 @@
 import { createIconifyOfflineIcon } from '@vben-core/icons';
 
 import inboxIcon from '@iconify/icons-ant-design/inbox-outlined';
+import defaultFileIcon from '@iconify/icons-bx/file';
+import sqlIcon from '@iconify/icons-carbon/sql';
 import folderIcon from '@iconify/icons-flat-color-icons/folder';
+import vueIcon from '@iconify/icons-logos/vue';
 import menuIcon from '@iconify/icons-material-symbols/menu';
 import okButtonIcon from '@iconify/icons-mdi/button-pointer';
+import javaIcon from '@iconify/icons-skill-icons/java-light';
+import tsIcon from '@iconify/icons-skill-icons/typescript';
+import xmlIcon from '@iconify/icons-tabler/file-type-xml';
 import excelIcon from '@iconify/icons-vscode-icons/file-type-excel';
 
 // 菜单类型 目录/按钮/菜单
@@ -31,4 +37,23 @@ export const InBoxIcon = createIconifyOfflineIcon(
 export const ExcelIcon = createIconifyOfflineIcon(
   'vscode-icons:file-type-excel',
   excelIcon,
+);
+
+export const JavaIcon = createIconifyOfflineIcon(
+  'skill-icons:java-light',
+  javaIcon,
+);
+export const XmlIcon = createIconifyOfflineIcon(
+  'tabler:file-type-xml',
+  xmlIcon,
+);
+export const SqlIcon = createIconifyOfflineIcon('carbon:sql', sqlIcon);
+export const TsIcon = createIconifyOfflineIcon(
+  'skill-icons:typescript',
+  tsIcon,
+);
+export const VueIcon = createIconifyOfflineIcon('logos:vue', vueIcon);
+export const DefaultFileIcon = createIconifyOfflineIcon(
+  'flat-color-icons:folder',
+  defaultFileIcon,
 );
